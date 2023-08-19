c743dd4341b2612d6bd807e5d9992d1b5a411772
“yang.liu1@bytedance.com” <yang.liu1@bytedance.com>
2023-08-19 20:10:34 +0800

    update
    Change-Id: Iebb981c538da0b8dd4da558d3a1ed0ff2245e1d1

--- LeetCode/ReverseInteger.h
+++ LeetCode/ReverseInteger.h
@@ -18,6 +18,7 @@ namespace LeetCode {
 
     int ReverseInteger::reverse(int x) {
        int ans = 0;
+       cout << INT_MAX << "  " << INT_MIN << endl;
        while(x) {
            if (ans>INT_MAX/10 || ans<INT_MIN/10) return 0;
            ans = ans * 10 + x % 10;
